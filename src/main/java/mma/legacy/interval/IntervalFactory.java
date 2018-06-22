cad7f019b564aa07cfb2fadc069c87ad5f9043ff
Iker.Garro <Iker.Garro@gmail.com>
2018-06-22 19:14:14 +0200

    Remove a.txt

--- src/main/java/mma/legacy/interval/IntervalFactory.java
+++ src/main/java/mma/legacy/interval/IntervalFactory.java
@@ -1,15 +1,14 @@
 package mma.legacy.interval;
 
-
 /**
- * Esta clase abstracta se ha creado por comodidad. 
- * Crea automáticamente intervalos
+ * Esta clase abstracta se ha creado por comodidad. Crea automáticamente intervalos
+ * 
  * @author Agustin
  *
  */
 public class IntervalFactory {
 
-	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
-	}
+    public static Interval getInterval(double minimum, double maximum, Opening opening) {
+        return new Interval(minimum, maximum, opening);
+    }
 }
